0f02475c94c882a0b0c28796293b14729462c098
kpilszak <klaudia.pilszak@gmail.com>
2020-08-10 20:37:56 +0200

    Create repo

---
+++ src/main/java/com/kpilszak/touroperator/TourOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.touroperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourOperatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpilszak/touroperator/TourOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.touroperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
