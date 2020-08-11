86dd95f5ff2c914426ac1b428215fb49c9831a63
kpilszak <klaudia.pilszak@gmail.com>
2020-08-11 22:11:37 +0200

    feat: Create services

---
+++ src/main/java/com/kpilszak/touroperator/services/TourPackageService.java
@@ -0,0 +1,31 @@
+package com.kpilszak.touroperator.services;
+
+import com.kpilszak.touroperator.domain.TourPackage;
+import com.kpilszak.touroperator.repo.TourPackageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourPackageService {
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourPackageService(TourPackageRepository tourPackageRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    public TourPackage createTourPackage(String code, String name) {
+        if (!tourPackageRepository.existsById(code)) {
+            tourPackageRepository.save(new TourPackage(code, name));
+        }
+        return null;
+    }
+
+    public Iterable<TourPackage> lookup() {
+        return tourPackageRepository.findAll();
+    }
+
+    public long total() {
+        return tourPackageRepository.count();
+    }
+}
---
+++ src/main/java/com/kpilszak/touroperator/services/TourService.java
@@ -0,0 +1,43 @@
+package com.kpilszak.touroperator.services;
+
+import com.kpilszak.touroperator.domain.Difficulty;
+import com.kpilszak.touroperator.domain.Region;
+import com.kpilszak.touroperator.domain.Tour;
+import com.kpilszak.touroperator.domain.TourPackage;
+import com.kpilszak.touroperator.repo.TourPackageRepository;
+import com.kpilszak.touroperator.repo.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TourService {
+    private TourPackageRepository tourPackageRepository;
+    private TourRepository tourRepository;
+
+    @Autowired
+    public TourService(TourPackageRepository tourPackageRepository, TourRepository tourRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+        this.tourRepository = tourRepository;
+    }
+
+    public Tour createTour(String title, String description, String blurb, Integer price, String duration, String bullets,
+                String keywords, String tourPackageCode, Difficulty difficulty, Region region) {
+        Optional<TourPackage> tourPackage = tourPackageRepository.findById(tourPackageCode);
+        if (!tourPackage.isPresent()) {
+            throw new RuntimeException("Tour package does not exist: " + tourPackageCode);
+        }
+
+        return tourRepository.save(new Tour(title, description, blurb, price, duration, bullets, keywords, tourPackage.get(),
+                difficulty, region));
+    }
+
+    public Iterable<Tour> lookup() {
+        return tourRepository.findAll();
+    }
+
+    public long total() {
+        return tourRepository.count();
+    }
+}
